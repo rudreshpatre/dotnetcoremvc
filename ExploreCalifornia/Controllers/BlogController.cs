7269802c010a1ba98ab59f452e7df3b9c1b743ed
Rudresh Patre <rudreshpatre@gmail.com>
2020-09-26 21:06:20 +0530

    Customize application URl's

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -6,18 +6,19 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ExploreCalifornia.Controllers
 {
+    [Route("Blog")]
     public class BlogController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Post(int? id) 
-        { 
-            if (id == null)
-                return new ContentResult { Content = "Pass Id in correct format."};
-            return new ContentResult{Content = id.ToString() };
+        [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
+        public IActionResult Post(int year,int month, string key) 
+        {            
+            return new ContentResult{Content = string.Format("Year: {0}; Month: {1}; Key: {2}",year,month,key)};
         }
     }
 }
