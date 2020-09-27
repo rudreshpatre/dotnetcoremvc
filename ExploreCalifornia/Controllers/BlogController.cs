e9cf9d3aacf13c9daf4874e02a3fd6f15c30bd03
Rudresh Patre <rudreshpatre@gmail.com>
2020-09-27 06:29:02 +0530

    Strongly typed views.

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExploreCalifornia.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExploreCalifornia.Controllers
 {
-    [Route("Blog")]
+    //[Route("Blog")]
     public class BlogController : Controller
     {
 
@@ -15,14 +16,18 @@ namespace ExploreCalifornia.Controllers
             return View();
         }
 
-        [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
-        public IActionResult Post(int year,int month, string key) 
+        //[Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
+        public IActionResult Post(int year, int month, string key)
         {
-            ViewBag.Title = "My Blog Post";
-            ViewBag.Posted = DateTime.Now;
-            ViewBag.Title = "Rudresh Patre";
-            ViewBag.Title = "This is a great blog post.";
-            return View();
-        }
+            var post = new Post()
+            {
+                Title = "My Blog Post",
+                Posted = DateTime.Now,
+                Author = "Rudresh Patre",
+                Body = "This is a great blog post."
+            };
+            return View(post);
+        }          
+            
     }
 }
