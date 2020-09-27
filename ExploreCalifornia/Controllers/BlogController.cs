40f38e0c155674e7381c460f8d74b97116cdeb66
Rudresh Patre <rudreshpatre@gmail.com>
2020-09-27 05:48:57 +0530

    Reuse shared markup with layout.

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -12,13 +12,13 @@ namespace ExploreCalifornia.Controllers
 
         public IActionResult Index()
         {
-            return new ContentResult {Content = "Blog View" };
+            return View();
         }
 
         [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year,int month, string key) 
-        {            
-            return new ContentResult{Content = string.Format("Year: {0}; Month: {1}; Key: {2}",year,month,key)};
+        {
+            return View();
         }
     }
 }
