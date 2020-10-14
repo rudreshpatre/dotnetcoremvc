6dc5a14e542ab1baa88205ea82378cbf8d60a6ca
Rudresh Patre <rudreshpatre@gmail.com>
2020-10-14 17:11:30 +0530

    Creating ViewComponents

---
+++ ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.ViewComponents
+{
+    [ViewComponent]
+    public class MonthlySpecialsViewComponent:ViewComponent
+    {
+        private readonly BlogDataContext _db;
+
+        public MonthlySpecialsViewComponent(BlogDataContext db)
+        {
+            _db = db;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var specials = _db.MonthlySpecials.ToArray();
+            return View(specials);
+        }
+    }
+}
